49a00e2ef4b8c2a40477f6238124b6199eb04bcd
RazielFa <razi.kaiser11@gmail.com>
2026-05-10 22:42:47 -0600

    Creación del validador para Login

---
+++ MindWeaveCliente/Validators/LoginValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MindWeaveClient.Properties.Langs;
+using MindWeaveClient.ViewModel.Authentication;
+
+namespace MindWeaveClient.Validators
+{
+    public class LoginValidator : AbstractValidator<LoginViewModel>
+    {
+        private const int MAX_EMAIL_LENGTH = 45;
+        private const int MAX_PASSWORD_LENGTH = 128;
+
+        public LoginValidator()
+        {
+            RuleFor(vm => vm.Email)
+                .NotEmpty().WithMessage(Lang.ValidationEmailRequired)
+                .EmailAddress().WithMessage(Lang.ValidationEmailInvalid)
+                .MaximumLength(MAX_EMAIL_LENGTH);
+
+            RuleFor(vm => vm.Password)
+                .NotEmpty().WithMessage(Lang.ValidationPasswordRequired)
+                .MaximumLength(MAX_PASSWORD_LENGTH);
+        }
+    }
+}
